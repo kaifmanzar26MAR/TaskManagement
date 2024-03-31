437060b0054e0b4623aec1762dee7fc7b6772fac
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-31 17:51:54 +0530

    f

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://task-management-project-ten.vercel.app",
     credentials: true,
   })
 );
