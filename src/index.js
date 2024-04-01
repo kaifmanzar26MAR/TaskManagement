7a626ab4ad91baff40d4151e18ea268157de6b85
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-04-01 23:09:59 +0530

    ddupdate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
-import { app } from "./app.js";
+import { app } from "./src/app.js"
 
-import connectDB from "./db/index.js";
+import connectDB from "./src/db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: ".env",
 });
 
 connectDB()
